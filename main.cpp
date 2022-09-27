58364118e0f182f640d485ae2560794c0f11200a
tanguy chappuis <tchappui@student.42lausanne.ch>
2022-09-27 23:25:14 +0200

    test

--- main.cpp
+++ main.cpp
@@ -10,27 +10,43 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <iostream>
-#include <string>
-#include <sys/socket.h>
-#include <sys/types.h>
-#include <arpa/inet.h>
+#include "server.h"
+
+t_pars	parsing(int ac, char **av)
+{
+	t_pars pars;
+	if (ac == 1)
+	{
+		pars.ip = "127.0.0.1";
+		pars.port = 2000;
+		pars.s_name = "tanguy";
+		pars.body = 2000; 
+	}
+	return (pars);
+}
 
 int main(int ac, char **av)
 {
-	(void)av;
-	if (ac != 2)
-		return (0);
+	t_pars pars;
+	pars = parsing(ac, av);
+	std::cout << pars.port << std::endl << pars.ip << std::endl;
 	int socketServer = socket(AF_INET, SOCK_STREAM, 0);
 	struct sockaddr_in addrServer;
-	addrServer.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+	addrServer.sin_addr.s_addr = inet_addr(pars.ip.c_str());
 	addrServer.sin_family = AF_INET;
-	addrServer.sin_port = htons(1000);
+	addrServer.sin_port = htons(pars.port);
 
 	bind(socketServer, (const struct sockaddr *)&addrServer, sizeof (addrServer));
-	listen(socketServer, 1);
+	std::cout << "bind : " << socketServer << std::endl;
+
+	listen(socketServer, 5);
+	std::cout << "listen" << std::endl;
 
 	struct sockaddr_in addrClient;
 	socklen_t csize = sizeof(addrClient);
 	int socketClient = accept(socketServer, (struct sockaddr *)&addrClient, &csize);
+	std::cout << "accept" << std::endl;
+
+	std::cout << "client : " << socketClient << std::endl;
 }
---
+++ server.h
@@ -0,0 +1,19 @@
+#ifndef SERVER_H
+# define SERVER_H
+
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <arpa/inet.h>
+
+typedef struct s_pars
+{
+	std::string	ip;
+	int			port;
+	int			body;
+	std::string	s_name;
+}	t_pars;
+
+#endif
